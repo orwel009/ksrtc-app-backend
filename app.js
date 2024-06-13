caa3d0dbeca48e71cd4cfff631bb454a90c3f2c3
orwel009 <orwelvincent1@gmail.com>
2024-06-14 00:19:57 +0530

    signin completed

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express=require("express")
 const mongoose=require("mongoose")
 const cors=require("cors")
 const bcryptjs = require("bcryptjs")
+const jwt = require("jsonwebtoken")
+
 
 const users = require("./models/users")
 const {userModel} = require("./models/users")
@@ -26,6 +28,36 @@ app.post("/signup",async(req,res)=>{
     res.json({"status":"success"})
 })
 
+app.post("/signin",(req,res)=>{
+    let input = req.body
+    userModel.find({"email":input.email}).then(
+      (response)=>{
+          if (response.length>0) {
+              let dbpassword = response[0].password
+              bcryptjs.compare(input.password,dbpassword,(error,isMatch)=>{
+                  if (isMatch) {
+                      jwt.sign({email:input.email},"blog-app",{expiresIn:"1d"},
+                          (error,token)=>{
+                          if (error) {
+                              res.json({"status":"unable to create token"})
+                          } else {
+                              res.json({"status":"succes","userId":response[0]._id,"token":token})
+                          }
+                      })
+                  } else {
+                      res.json({"status":"Invalid credentials"})
+                  }
+              })
+          } else {
+              res.json({"status":"user not found"})
+          }
+      }
+    ).catch(
+      (error)=>{
+          res.json(error)
+      }
+    )  
+  })
 
 app.listen(8080,()=>{
     console.log("Server Started")
