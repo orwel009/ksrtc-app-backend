3c2bfe8bb6b03a4b724c71ca2e84c6a244bb0615
orwel009 <orwelvincent1@gmail.com>
2024-06-14 00:49:00 +0530

    addBus completed

--- models/bus.js
+++ models/bus.js
@@ -4,8 +4,9 @@ const schema = mongoose.Schema(
         "busNo":{type:String,required:true},
         "busName":{type:String,required:true},
         "busRoot":{type:String,required:true},
-        "driverName":{type:String,required:true}    }
+        "driverName":{type:String,required:true}    
+    }
 )
 
 const busModel = mongoose.model("buses",schema)
-module.exports={busModelModel}
+module.exports={busModel}
